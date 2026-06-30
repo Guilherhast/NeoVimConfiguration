2dd21e917adf72769691642ef78de5e6cff53e49
Guilherme Hastenreiter Ferreira <hast.job@gmail.com>
2026-06-30 10:44:31 -0300

    Refac: Changed some general settings

--- init.lua
+++ init.lua
@@ -1,6 +1,2 @@
--- Match \t*\s* the \s should be red
--- Make it a command
--- Show registers and paste the grepped
--- Create a command to clean a list of bible verses
 require('GuilherHast')
 require('Plugins')
